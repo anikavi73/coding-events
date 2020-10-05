937024ae59eba3196a89293f86158ec5f4f04839
Kavitha <anikavi73@gmail.com>
2020-10-05 10:03:04 -0500

    model-binding

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -1,11 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,16 +16,51 @@ import java.util.Map;
 
 public class EventsController {
 
+//    private static List<Event> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
+      model.addAttribute("title", "All Events");
+//      model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
+      return "events/index";
+    }
 
-        Map<String, String> eventsCode = new HashMap<>();
-        eventsCode.put("Code with Pride", "LGBT Friendly Coding meetup");
-        eventsCode.put("Strange Loop", "Loops that are strange");
-        eventsCode.put("Apple WWDC", "Event for people with pricey computers");
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model){
+        model.addAttribute("title", "Create Events");
+        return "events/create";
+    }
 
-        model.addAttribute("events", eventsCode);
+//   @PostMapping("create")
+//    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription){
+////        events.add(new Event(eventName, eventDescription));
+//        EventData.add(new Event(eventName, eventDescription));
+//        return "redirect:";
+//    }
 
-        return "events/index";
+    //Using Model-Binding by using  Spring will create the newEvent instrance
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute Event newEvent){
+//        events.add(new Event(eventName, eventDescription));
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds){
+        if(eventIds!= null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+        return "redirect:";
     }
 }
