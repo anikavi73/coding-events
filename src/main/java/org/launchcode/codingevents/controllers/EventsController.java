98e1e6a876ad10ca1f8328ace3756536670d63ae
Kavitha <anikavi73@gmail.com>
2020-10-06 21:28:59 -0500

    validation

--- src/main/java/org/launchcode/codingevents/controllers/EventsController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventsController.java
@@ -4,45 +4,42 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
 
 public class EventsController {
 
-//    private static List<Event> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model){
       model.addAttribute("title", "All Events");
-//      model.addAttribute("events", events);
-        model.addAttribute("events", EventData.getAll());
+      model.addAttribute("events", EventData.getAll());
       return "events/index";
     }
 
     @GetMapping("create")
     public String displayCreateEventForm(Model model){
         model.addAttribute("title", "Create Events");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
-//   @PostMapping("create")
-//    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription){
-////        events.add(new Event(eventName, eventDescription));
-//        EventData.add(new Event(eventName, eventDescription));
-//        return "redirect:";
-//    }
-
-    //Using Model-Binding by using  Spring will create the newEvent instrance
+    //Using Model-Binding by using  Spring will create the newEvent instance
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent){
-//        events.add(new Event(eventName, eventDescription));
+//    public String processCreateEventForm(@ModelAttribute Event newEvent){
+    // add valid and errors and if loop
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+
+        if (errors.hasErrors()){
+            model.addAttribute("title", "Create Events");
+//            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:";
     }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,18 +1,34 @@
 package org.launchcode.codingevents.models;
 
 import java.util.Objects;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class Event {
 
     private int id;
     private static int nextId =1;
 
+    public Event(){
+        this.id=nextId;
+    }
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max =50, message = "Name is between 3 to 50 characters long!" )
     private String name;
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -35,6 +51,14 @@ public class Event {
 
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
